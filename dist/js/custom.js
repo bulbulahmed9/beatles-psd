79cef7aa76cf9ebd3a55d7d704f9ab88ff185d60
unknown <bulbulahmed.tk@gmail.com>
2019-09-25 05:31:44 -0700

    first commit

---
+++ dist/js/custom.js
@@ -0,0 +1,8 @@
+$('.js-about').waypoint(function(direction){
+    if(direction == 'down'){
+        $('nav').addClass('sticky');
+    }
+    else{
+        $('nav').removeClass('sticky');
+    }
+});
